61346f312b5f6bb15dbb51e65383a788663546df
manmanlan <prikitiki860@gmail.com>
2024-12-29 23:56:05 +0100

    PROCEDUAL map

--- dungeon-rogue/map.gd
+++ dungeon-rogue/map.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+var floor_tile := Vector2i(0,0)
+
+
+func _ready() -> void:
+	var tile_position=Vector2i(0,0)
+	for v1 in range(41):
+		
+		for v2 in range(72):
+			tile_map_layer.set_cell(tile_position,4,floor_tile)
+			tile_position += Vector2i(1,0)
+		tile_position = Vector2i(0, tile_position.y + 1)
