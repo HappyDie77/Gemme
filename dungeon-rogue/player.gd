213ee5217a5810a1bd4fe8abf6d120c3fec45479
manmanlan <prikitiki860@gmail.com>
2025-01-02 01:30:50 +0100

    sigma sigma boy

--- dungeon-rogue/player.gd
+++ dungeon-rogue/player.gd
@@ -3,7 +3,17 @@ extends CharacterBody2D
 var SPEED = 100.0
 
 func _physics_process(delta: float) -> void:
-	# Cleaner movement using Input.get_vector()
+	
 	velocity = Input.get_vector("a", "d", "w", "s") * SPEED
+	
+	
+	if Input.is_action_pressed("a"):
+		$AnimatedSprite2D.play("left")
+	elif Input.is_action_pressed("d"):
+		$AnimatedSprite2D.play("right")
+	elif Input.is_action_pressed("w"):
+		$AnimatedSprite2D.play("up")
+	else:
+		$AnimatedSprite2D.play("default")
 	
 	move_and_slide()  # Move the player
